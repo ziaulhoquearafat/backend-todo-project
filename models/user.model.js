8528ed6c2a5e4e72713556b30ada755d714b3474
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-05-17 23:48:30 +0600

    update user model schema with validation rules

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,22 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-
+    fullName: {
+        type:String,
+        required:[true, 'Please provide your full name']
+    },
+    email:{
+        type:String,
+        required:[true, 'please enter a valid email address'],
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:[true, 'Password must be at least 6 characters long'],
+        minLength:6,
+    }
 })
 
 const User = mongoose.model('User', userSchema);
 
-export default User;
+export default User;    
