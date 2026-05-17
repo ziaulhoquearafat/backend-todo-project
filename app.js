d49791d40f171387fac917a6ff16080a5f7a1b97
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-05-17 23:41:26 +0600

    refactor: clean up app.js and add user model

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/databse.js';
-
 const app = express();
 dotenv.config();
-
 connectDB();
 
 const PORT = process.env.PORT || 8000;
