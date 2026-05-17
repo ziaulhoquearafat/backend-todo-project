18a91ed5e73fecef7e60a40976e80ee35fc9054e
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-05-18 01:39:00 +0600

    add user registration route and bcryptjs dependency

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
 import connectDB from './db/databse.js';
+import userRoutes from './routes/user.routes.js';
 const app = express();
 dotenv.config();
 connectDB();
 
+app.use("/api/v1/users", userRoutes);
+
 const PORT = process.env.PORT || 8000;
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`)
 })
