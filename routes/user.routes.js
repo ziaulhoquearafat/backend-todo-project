80635eb088b31dac040b8e67a95fd5b73d373ecd
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-05-20 02:28:21 +0600

    refactor user routes to include login endpoint and correct registration route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerUser } from '../controllers/user.controllers.js';
+import { login, registerUser } from '../controllers/user.controllers.js';
 
 const router = express.Router();
 
-router.post('/', registerUser);
+router.post('/register', registerUser);
+router.post('/login', login);
 
 export default router;
